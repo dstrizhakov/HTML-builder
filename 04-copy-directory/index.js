ab80a7a84a80c75916b702ba2fc8dc193b65fa29
dstrizhakov <dstrizhakov@gmail.com>
2023-05-06 15:10:57 +0300

    feat: add solution to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require("fs");
+const path = require("path");
 
+const fromFolder = path.join(__dirname, "files");
+const toFolder = path.join(__dirname, "files-copy");
+
+fs.rm(toFolder, { recursive: true, force: true }, (error) => {
+  !error ? console.log("Folder deleted successfully") : console.error(error);
+  fs.mkdir(toFolder, { recursive: true }, (error) => {
+    !error ? console.log("Folder created successfully") : console.error(error);
+    fs.readdir(fromFolder, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        console.error(error);
+      } else {
+        for (let file of files) {
+          if (file.isFile()) {
+            fs.copyFile(
+              path.join(fromFolder, file.name),
+              path.join(toFolder, file.name),
+              (error) => {
+                error
+                  ? console.error(error)
+                  : console.log(`File ${file.name} copied successfully`);
+              }
+            );
+          }
+        }
+      }
+    });
+  });
+});
